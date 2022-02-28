b21ca7af8f1b2106ec5899be6f4f1f3e9b8f0c78
darentanrw <daren28@gmail.com>
2022-03-01 00:34:11 +0800

    picture taking loop, remove old files

--- fswebcam.py
+++ fswebcam.py
@@ -1,3 +1,3 @@
 import os
 
-os.system('fswebcam -r 1280x720 --no-banner --save /home/pi/Documents/huawei-hackathon/raspberry-pi/images/image1.jpg')
+os.system('fswebcam -r 1280x720 --no-banner --save /home/pi/Documents/huawei-hackathon/rpi/images/image1.jpg')
--- webcam.py
+++ webcam.py
@@ -1,11 +1,33 @@
-from cv2 import *
-# initialize the camera
-cam = VideoCapture(0)   # 0 -> index of camera
-s, img = cam.read()
 
-if s:    # frame captured without any errors
-    namedWindow("cam-test",WINDOW_AUTOSIZE)
-    imshow("cam-test",img)
-    waitKey(0)
-    destroyWindow("cam-test")
-    imwrite("filename.jpg",img) #save image
+import os, time
+from cv2 import *
+
+sleepTime = 5
+
+while True:
+    fileList = os.listdir('images')
+    fullPath = [f"images/{name}" for name in fileList]
+
+    if len([name for name in fileList]) == 10:
+        oldestFile = min(fullPath, key=os.path.getctime)
+        os.remove(oldest_file)
+
+    if len([name for name in fileList]) == 0:
+        count = 0
+    else:
+        newestFile = max(fullPath, key-os.path.getctime(path))
+        count = int(newestFile.split('/')[-1].split('.')[0])
+
+    cam = VideoCapture(0) 
+    s, img = cam.read()
+
+    time.sleep(sleepTime)
+
+    if s:    # frame captured without any errors
+        # namedWindow("cam-test",WINDOW_AUTOSIZE)
+        # imshow("cam-test",img)
+        # waitKey(0)
+        # destroyWindow("cam-test")
+        
+        imwrite(f"images/image_{count}",img) #save image
+        count += 1
