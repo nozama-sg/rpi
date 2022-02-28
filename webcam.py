966afdea40e9acf7cf06838c89e9c7c57c6cee1b
darentanrw <daren28@gmail.com>
2022-03-01 00:15:40 +0800

    minor bug fix

--- webcam.py
+++ webcam.py
@@ -4,7 +4,7 @@ cam = VideoCapture(0)   # 0 -> index of camera
 s, img = cam.read()
 
 if s:    # frame captured without any errors
-    namedWindow("cam-test",CV_WINDOW_AUTOSIZE)
+    namedWindow("cam-test",WINDOW_AUTOSIZE)
     imshow("cam-test",img)
     waitKey(0)
     destroyWindow("cam-test")
