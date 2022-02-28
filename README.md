6fa0fb566d857152cb00cb10df74bf29488684b9
darentanrw <daren28@gmail.com>
2022-02-28 23:56:54 +0800

    cv2 and fswebcam

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rpi
 Repository for Raspberry Pi
+
+
+Note: This build is in a linux environment (Raspbian) for Raspberry Pi 2B (armv7l)
---
+++ fswebcam.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('fswebcam -r 1280x720 --no-banner --save /home/pi/Documents/huawei-hackathon/raspberry-pi/images/image1.jpg')
