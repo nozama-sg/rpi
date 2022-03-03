a07c9b905a546c6f39c13dd77c18262fd26363da
darentanrw <daren28@gmail.com>
2022-03-03 17:33:45 +0800

    routes implemented

--- mqtt-bluetooth/client.py
+++ mqtt-bluetooth/client.py
@@ -2,10 +2,14 @@ import paho.mqtt.client as paho
 import sys
 import time
 import json
+import datetime
 
-mqttServerIP = 'localhost'
-# mqttServerIP = '192.168.1.104'
+# mqttServerIP = 'localhost'
+mqttServerIP = '192.168.1.104'
+userId = "darentan"
 deviceName = 'iBeacon:c80c71ef-1086-4601-9dc1-c83eadb4be7c-0-0'
+bluetoothUpdateURL = "http://119.13.104.214:80/locationUpdate"
+updateDelayTime = 10
 
 valuesDict = {}
 
@@ -16,7 +20,7 @@ def onMessage(client, userdata, message):
     rssi = message['rssi']
     mac = message['mac']
 
-    # print(f"LOG: {baseStation}:{deviceId} RSSI: {rssi} MAC: {mac}")
+    print(f"LOG: {baseStation}:{deviceId} | RSSI: {rssi} | MAC: {mac}")
 
     # we can incorporate the deviceId to enable multiple watch support
     valuesDict[baseStation] = rssi
@@ -24,7 +28,22 @@ def onMessage(client, userdata, message):
 
     # get key in valuesDict with highest value
     maxBaseStation = max(valuesDict, key=valuesDict.get)
-    print(maxBaseStation)  
+    print(maxBaseStation)
+
+    now = datetime.datetime.now().strftime('%Y-%m-%d %X')
+    
+    data = {
+        "userId": userId,
+        "roomName": maxBaseStation,
+        "timestamp": now
+    }
+
+    response = requests.post(bluetoothUpdateURL, json=data)
+
+    if response.status_code != 200:
+        print(f"ERROR: {response.status_code} | {response.text}")
+
+    time.sleep(updateDelayTime)
     
 def onLog(client, userdata, level, buf):
     print(f"LOG: {buf}")
@@ -34,7 +53,7 @@ client.on_message = onMessage
 # client.on_log = onLog
 
 if client.connect(mqttServerIP, 1883) != 0:
-    print("Could not connect to MQTT Broker!")
+    print("Could not connect to MQTT Broker")
     sys.exit(-1)
 
 client.subscribe(f"espresense/devices/{deviceName}/#")
@@ -44,6 +63,5 @@ try:
     client.loop_forever()
 except:
     print("Disconnecting from broker")
-
 
 client.disconnect()
